95c10f0705011e2bdf52d0155461f1e45f23e137
adimpactAI <165220688+adimpactcode@users.noreply.github.com>
2024-08-08 11:06:24 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,42 +7,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>
 );
-import React, { useEffect, useRef } from 'react';
-import { Webchat, WebchatProvider, useClient } from '@botpress/webchat';
-
-import './style.css';
-import { theme } from './theme';
-
-const clientId = 'bfaf7c32-f481-4cf7-99ab-0ffe76b6e322';
-
-export default function App() {
-  const client = useClient({ clientId });
-  const chatContainerRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const chatContainer = chatContainerRef.current;
-    if (chatContainer) {
-      // Scrollt zum Ende des Containers, wenn eine neue Nachricht hinzukommt
-      const observer = new MutationObserver(() => {
-        chatContainer.scrollTo({
-          top: chatContainer.scrollHeight,
-          behavior: 'smooth',
-        });
-      });
-
-      observer.observe(chatContainer, { childList: true, subtree: true });
-
-      return () => {
-        observer.disconnect();
-      };
-    }
-  }, []);
-
-  return (
-    <WebchatProvider client={client} theme={theme}>
-      <div ref={chatContainerRef} className="bpw-chat-container">
-        <Webchat />
-      </div>
-    </WebchatProvider>
-  );
 }
